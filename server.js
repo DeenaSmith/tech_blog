e6378be56ce85927311b7bf56e3b5eb25a4c9a43
Deena Smith <deenasmith1630@gmail.com>
2022-05-11 15:03:11 -0500

    corrected files, added login and post options for user

--- server.js
+++ server.js
@@ -27,11 +27,9 @@ const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use(require("./controllers/"));
 
 sequelize.sync({ force: false }).then(() => {
